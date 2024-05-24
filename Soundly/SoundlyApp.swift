15bc5867bebaf1dbd071dd7895ecd4373a0d3eb4
lakshay <gupta22154006@akgec.ac.in>
2024-05-24 11:55:01 +0530

    Initial Commit

---
+++ Soundly/SoundlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundlyApp.swift
+//  Soundly
+//
+//  Created by Lakshay Gupta on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
