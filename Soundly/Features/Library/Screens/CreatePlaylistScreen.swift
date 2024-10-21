1796afc30eb25f2cd71d136c95d1fe3cd65872bd
lakshay <gupta22154006@akgec.ac.in>
2024-10-22 02:07:51 +0530

    Add assets for CreatePlaylistIllustration and CreatePlaylistScreen

---
+++ Soundly/Features/Library/Screens/CreatePlaylistScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePlaylistScreen.swift
+//  Soundly
+//
+//  Created by Lakshay Gupta on 09/06/24.
+//
+
+import SwiftUI
+
+struct CreatePlaylistScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePlaylistScreen()
+}
