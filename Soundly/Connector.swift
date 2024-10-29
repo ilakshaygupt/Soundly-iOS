6f7524785f7f96405931cfb372c3fdd996bd9735
lakshay <gupta22154006@akgec.ac.in>
2024-10-29 17:41:13 +0530

    Refactor PlayerControls in SongSheetView.swift

--- Soundly/Connector.swift
+++ Soundly/Connector.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import WatchConnectivity
+
+class WatchConnection : NSObject, WCSessionDelegate{
+
+    var session:WCSession
+
+    init (session:WCSession = .default){
+        self.session = session
+        super.init()
+        session.delegate = self
+        session.activate()
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {
+
+    }
+
+    func sessionDidBecomeInactive(_ session: WCSession) {
+
+    }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+
+    }
+
+}
--- Soundly/Features/Auth/GetStarted/Screens/GetStartedScreen.swift
+++ Soundly/Features/Auth/GetStarted/Screens/GetStartedScreen.swift
@@ -7,44 +7,63 @@
 
 import SwiftUI
 struct GetStartedScreen: View {
+    @State var isGetStartedClicked : Bool = false
+    
 
 
     var body: some View {
         NavigationStack {
-            GeometryReader { geometry in
-                VStack {
-                    HStack {
-                        Text("Your Thoughts")
-                            .font(.system(size: 30))
-                        Text("Cleaner")
-                            .foregroundColor(Color(red: 0.0, green: 0.545, blue: 0.545))
-                            .font(.system(size: 30))
-                            .bold()
-                    }
-                    .padding(.top, 40)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    
-                    Text("Let the music take control!")
-                        .font(.system(size: 20))
-                        .padding(.top, 16)
+            if isGetStartedClicked{
+                LoginScreen()
+
+            }
+            else{
+                GeometryReader { geometry in
+                    VStack {
+                        HStack {
+                            Text("Your Thoughts")
+                                .font(.system(size: 30))
+                            Text("Cleaner")
+                                .foregroundColor(Color(red: 0.0, green: 0.545, blue: 0.545))
+                                .font(.system(size: 30))
+                                .bold()
+                        }
+                        .padding(.top, 40)
                         .frame(maxWidth: .infinity, alignment: .center)
-                    
-                    Image("Yoga")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: getScreenBounds().width  * 0.90, height: getScreenBounds().height  * 0.66)
-                    Spacer()
-                    NavigationLink(destination: LoginScreen()) {
-                        Text("Get Started →")
-                            .frame(width: getScreenBounds().width * 0.70)
+
+                        Text("Let the music take control!")
                             .font(.system(size: 20))
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color(red: 0.0, green: 0.545, blue: 0.545))
-                            .cornerRadius(10)
+                            .padding(.top, 16)
+                            .frame(maxWidth: .infinity, alignment: .center)
+
+                        Image("Yoga")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: getScreenBounds().width  * 0.90, height: getScreenBounds().height  * 0.66)
+                        Spacer()
+                        Button(
+                            action:{
+                                    withAnimation{
+                                        isGetStartedClicked.toggle()
+
+                                    }
+
+                                                            }
+
+                        ){
+                            Text("Get Started →")
+                                .frame(width: getScreenBounds().width * 0.70)
+                                .font(.system(size: 20))
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color(red: 0.0, green: 0.545, blue: 0.545))
+                                .cornerRadius(10)
+
+                        }
+                        .padding(.bottom , 50)
+
                     }
-                    .padding(.bottom , 50)
                 }
             }
         }
