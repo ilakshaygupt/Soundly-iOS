b2635ddb592a6920f886e475f7dfe62dcf4e204c
lakshay <gupta22154006@akgec.ac.in>
2024-10-29 17:39:14 +0530

    Added setup files for SoundlyWatchOs Watch App

---
+++ SoundlyWatchOs Watch App/SoundlyWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundlyWatchOsApp.swift
+//  SoundlyWatchOs Watch App
+//
+//  Created by Lakshay Gupta on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundlyWatchOs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
